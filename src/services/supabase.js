f85121523c2a57e377304e335d706e9f5fccd271
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2026-05-18 13:45:25 +0300

    added env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://rngsxemvajihxgcznqrx.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuZ3N4ZW12YWppaHhnY3pucXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MDA0MTAsImV4cCI6MjA4ODk3NjQxMH0.NlsBOgFbY-QMh25A19Nm8mOm2oAKiQIUrOtjCO1KDcU";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
-
-// const supabaseKey = process.env.SUPABASE_KEY;
