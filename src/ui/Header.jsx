e0718f664c734a3f21f5b2bb13d709add9fca425
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2026-07-26 13:00:43 +0300

    ADDED: app header, update user data and password

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,22 @@
 import styled from 'styled-components'
-import Logout from '../features/authentication/Logout'
+import HeaderMenu from './HeaderMenu'
+import UserAvatar from '../features/authentication/UserAvatar'
 
 const StyledHeader = styled.header`
     background-color: var(--color-grey-0);
     padding: 1.2rem 2.8rem 3.4rem;
-border-bottom: 1px solid #1a0e0e;
+    border-bottom: 1px solid #1a0e0e;
+
+    display: flex;
+    gap: 2.4rem;
+    align-items: center;
+    justify-content: flex-end;
 `
 export default function Header() {
     return (
-        <StyledHeader><Logout /></StyledHeader>
+        <StyledHeader>
+            <UserAvatar />
+            <HeaderMenu />
+        </StyledHeader>
     )
 }
---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+import Logout from "../features/authentication/Logout"
+import ButtonIcon from "./ButtonIcon"
+import { HiOutlineUser } from "react-icons/hi2"
+import { useNavigate } from "react-router-dom"
+
+const StyledHeadeMenu = styled.ul`
+    display: flex;
+    gap: 04 rem;
+`
+const HeaderMenu = () => {
+    const navigate = useNavigate();
+
+    return (
+        <StyledHeadeMenu>
+            <li>
+                <ButtonIcon onClick={() => navigate('/account')}>
+                    <HiOutlineUser />
+                </ButtonIcon>
+            </li>
+            <li>
+                <Logout />
+            </li>
+        </StyledHeadeMenu>
+    )
+}
+
+export default HeaderMenu
